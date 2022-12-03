a012eacd5717b3b2fafe0435870624f5d968071d
Aleksander Opałka <d3bian@mopalka.idl.pl>
2022-12-03 18:56:58 +0100

    Deletion of PasswordGenerator
    Deleted this due to fact that i had no big idea behind this so it was a bit meaning less. And this fact makes it very hard to create good code when you're not sure what it will be used for

--- PasswordGenerator/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(PasswordGenerator.generatePassword(15));
-        }
-    }
-}
